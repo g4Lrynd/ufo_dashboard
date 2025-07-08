16b3a7f9adb4275478e42c8ce8e4b592c8adf546
Zakariya <zakariya.bouzid@outlook.com>
2025-07-08 23:34:25 +0100

    Show a bar chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,89 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Rectangle,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 function App() {
+  const url =
+    "https://my-json-server.typicode.com/Louis-Procode/ufo-Sightings/ufoSightings";
+  const [ufos, setUfos] = useState<UfoSighting[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  interface UfoSighting {
+    date: string;
+    sightings: number;
+  }
+
+  const fetchUfos = async (): Promise<void> => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        setError(`Error ${response.status}`);
+        return;
+      }
+
+      const json: UfoSighting[] = await response.json();
+      setUfos(json);
+      setError(null);
+    } catch (error) {
+      setError((error as Error).message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUfos();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Something went wrong: {error}</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {ufos.length > 0 && (
+        <ResponsiveContainer width="100%" height={400}>
+          <BarChart
+            width={500}
+            height={300}
+            data={ufos}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar
+              dataKey="sightings"
+              fill="#8884d8"
+              activeBar={<Rectangle fill="#514aa5" />}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      )}
     </div>
   );
 }
